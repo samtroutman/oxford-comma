88a629d094a85732a159e5adee5d1ae8e4534517
Sam <sam@Sams-MacBook-Air.local>
2020-11-21 14:43:28 -0500

    commit tree 1e4232bb7ee8e62901dae3b1a6d8c0567d1d2f98

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
-end
+    if array.count == 1
+        return string = array.join()
+    elsif array.count ==2
+            string = array.join(" and ")
+    else
+            string = array[0..-2].join(", ")
+            string << ", and"
+            string << " #{array[-1]}"
+        end
+    end
+    
+    
+    
+    
+    
+    
+    
